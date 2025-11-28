45cf95cc6795596cb8665c20b0f608521f54338b
AshokBattula <ashok.bathula11@gmail.com>
2025-11-28 14:19:15 +0530

    Added driver.quit

--- src/test/java/TestCases/LoginTestCase.java
+++ src/test/java/TestCases/LoginTestCase.java
@@ -19,5 +19,6 @@ public class LoginTestCase {
     public void openGoogle(){
         WebDriver driver =new ChromeDriver();
         driver.get("https://google.com");
+        driver.quit();
     }
 }
